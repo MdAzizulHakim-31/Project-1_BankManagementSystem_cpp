c6bf91cd1d1f11453a18894638fb1fc58ad7e1fe
MdAzizulHakim-31 <aazizul924@gmail.com>
2023-06-26 07:17:43 +0600

    bank

--- Project_Bank-Management(Azizul).cpp
+++ Project_Bank-Management(Azizul).cpp
@@ -2,3 +2,24 @@
 #include<fstream>
 #include<iomanip>
 #include<cctype>
+
+using namespace std;
+class Bank_Account
+{
+    int Money_Deposit;
+    char type;
+    int accountNo;
+    char name[50];
+
+    public:
+
+    void report() const;
+	int ret_Money_Deposit() const;
+	void create_Bank_Account();
+	void deposit(int);
+	int ret_Acc_No() const;
+	void display_Account() const;
+	void update();
+	char ret_Type() const;
+	void withdraw(int);
+};
