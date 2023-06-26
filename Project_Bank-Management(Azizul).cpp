d70c92e08e7a2c160c239bbda0606f2cb4d83081
MdAzizulHakim-31 <aazizul924@gmail.com>
2023-06-26 09:29:05 +0600

    main functoin

--- Project_Bank-Management(Azizul).cpp
+++ Project_Bank-Management(Azizul).cpp
@@ -92,3 +92,93 @@ int Bank_Account::ret_Money_Deposit() const
 {
     return Money_Deposit;
 }
+
+
+// ...Main function
+void write_Bank_Account();
+void display_details(int);
+void display_all();
+void delete_Bank_Account(int);
+void Money_Deposit_withdraw(int, int);
+void update_Bank_Account(int);
+
+int main()
+{
+    char serial;
+    int num;
+    do
+    {
+        cout << "\n\n\t\t***WELCOME TO BANK MANAGEMENT SYSTEM***";
+        cout << "\n\n\t\t-------------------------------\n";
+        cout << "\t\t    ::MAIN MENU::";
+        cout << "\n\t\t-------------------------------";
+
+        cout << "\n\t\t1. NEW BANK ACCOUNT";
+        cout << "\n\t\t2. DEPOSIT";
+        cout << "\n\t\t3. WITHDRAW";
+        cout << "\n\t\t4. BALANCE ENQUIRY";
+        cout << "\n\t\t5. ALL BANK ACCOUNT HOLDER LIST";
+        cout << "\n\t\t6. CLOSE A BANK ACCOUNT";
+        cout << "\n\t\t7. UPDATE A BANK ACCOUNT";
+        cout << "\n\t\t8. EXIT";
+        cout << "\n\n\t\tSelect Your Option (1-8): ";
+        cin >> serial;
+
+        switch (serial)
+        {
+        case '1':
+            write_Bank_Account();
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '2':
+            system("CLS");
+            cout << "\n\n\tPlease Enter The Bank Account No. : ";
+            cin >> num;
+            Money_Deposit_withdraw(num, 1);
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '3':
+            system("CLS");
+            cout << "\n\n\tPlease Enter The Bank_Account No. : ";
+            cin >> num;
+            Money_Deposit_withdraw(num, 2);
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '4':
+            system("CLS");
+            cout << "\n\n\tPlease Enter The Bank_Account No. : ";
+            cin >> num;
+            display_details(num);
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '5':
+            display_all();
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '6':
+            system("CLS");
+            cout << "\n\n\tPlease Enter The Bank_Account No. : ";
+            cin >> num;
+            delete_Bank_Account(num);
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '7':
+            system("CLS");
+            cout << "\n\n\tPlease Enter The Bank_Account No. : ";
+            cin >> num;
+            update_Bank_Account(num);
+            cout << "\n\n\n\n\n\tPress 'Enter' To Continue...";
+            break;
+        case '8':
+            system("CLS");
+            cout << "\n\n\tThanks For Visiting My Bank Management System.";
+            cout << "\n\n\tCreated By Azizul Hakim.\n";
+            break;
+        default:
+            cout << "\a";
+        }
+        cin.ignore();
+        cin.get();
+    } while (serial != '8');
+    return 0;
+}
